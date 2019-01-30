56bdf4c2a80adde0c2d8dac8ac4aa9ca260d53f3
Leon <32615985+leonhess@users.noreply.github.com>
2019-01-30 23:32:44 +0100

    #1 lots of new stuff

--- website/app/routes.py
+++ website/app/routes.py
@@ -2,12 +2,17 @@ from flask import render_template
 from app import app
 from app import influx
 from app import sensors
+import dateutil.parser
 
 
 @app.route('/')
 def index():
-    user = {'username': 'Leon'}
 
+    return render_template('index.html')
+
+
+@app.route('/sensors')
+def table():
     temp_result = []
     hum_result = []
 
@@ -18,24 +23,26 @@ def index():
             query = influx.query("SELECT temperature FROM {} ORDER by time DESC LIMIT 1".format(measurement))
             raw = query.get_points()
             for res in raw:
+                time = dateutil.parser.parse(res['time'])
                 temp_result.append(
                     {
-                        "temperature": res['temperature'],
-                        "time": res['time'],
+                        "temperature": round(res['temperature'], 2),
+                        "time": "{}:{}.{}".format(time.hour, time.minute, time.second),
                         "name": sensor['nickname']
-                    }
+                   }
                 )
 
         if sensor['hum']:
             query2 = influx.query("SELECT humidity FROM {} ORDER by time DESC LIMIT 1".format(measurement))
             raw2 = query2.get_points()
             for res2 in raw2:
+                time = dateutil.parser.parse(res2['time'])
                 hum_result.append(
                     {
-                        "humidity": res2['humidity'],
-                        "time": res2['time'],
+                        "humidity": round(res2['humidity'], 2),
+                        "time": "{}:{}.{}".format(time.hour, time.minute, time.second),
                         "name": sensor['nickname']
                     }
                 )
 
-    return render_template('index.html', temp_result=temp_result, hum_result=hum_result)
+    return render_template('sensors.html', temp_result=temp_result, hum_result=hum_result)
