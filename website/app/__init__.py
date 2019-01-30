dea8f561951f75ecc4b49de21a7ee507311d32d7
Leon <32615985+leonhess@users.noreply.github.com>
2019-01-30 17:58:11 +0100

    #1 added humdity readings

--- website/app/__init__.py
+++ website/app/__init__.py
@@ -6,16 +6,31 @@ influx = InfluxDBClient(host="192.168.66.56", port=8086, database='smarthome')
 
 sensors = [
     {
-      "id": "01131657bc73",
-      "name": "front_window_outside"
+        "temp": True,
+        "hum": False,
+        "id": "01131657bc73",
+        "name": "front_window_outside",
+        "nickname": "Draußen"
     },
     {
-      "id": "011830cd8dff",
-      "name": "front_window_inside"
+        "temp": True,
+        "hum": False,
+        "id": "011830cd8dff",
+        "name": "front_window_inside",
+        "nickname": "vorderes Fensterbrett"
     },
     {
-      "id": "0113170ac3ed",
-      "name": "front_third"
+        "temp": True,
+        "hum": False,
+        "id": "0113170ac3ed",
+        "name": "front_third",
+        "nickname": "vordere Jalousie"
+    },
+    {
+        "temp": True,
+        "hum": True,
+        "name": "window_front",
+        "nickname": "vorderes Fensterbrett"
     }
 ]
 
--- website/app/routes.py
+++ website/app/routes.py
@@ -8,22 +8,34 @@ from app import sensors
 def index():
     user = {'username': 'Leon'}
 
-    result = []
-    result2 = []
+    temp_result = []
+    hum_result = []
 
     for sensor in sensors:
-        measurement = sensor['name']
-        query = influx.query("SELECT temperature FROM {} ORDER by time DESC LIMIT 1".format(measurement))
-        raw = query.get_points()
 
-        for res in raw:
-            result2.append(res)
-            result.append(
-                {
-                    "temperature": res['temperature'],
-                    "time": res['time'],
-                    "name": sensor['name']
-                }
-            )
+        if sensor['temp']:
+            measurement = sensor['name']
+            query = influx.query("SELECT temperature FROM {} ORDER by time DESC LIMIT 1".format(measurement))
+            raw = query.get_points()
+            for res in raw:
+                temp_result.append(
+                    {
+                        "temperature": res['temperature'],
+                        "time": res['time'],
+                        "name": sensor['nickname']
+                    }
+                )
 
-    return render_template('index.html', query=result)
+        if sensor['hum']:
+            query2 = influx.query("SELECT humidity FROM {} ORDER by time DESC LIMIT 1".format(measurement))
+            raw2 = query2.get_points()
+            for res2 in raw2:
+                hum_result.append(
+                    {
+                        "humidity": res2['humidity'],
+                        "time": res2['time'],
+                        "name": sensor['nickname']
+                    }
+                )
+
+    return render_template('index.html', temp_result=temp_result, hum_result=hum_result)
