ce8f75364c41659631414ed7268fa2e5f4fecc4b
Leon <32615985+leonhess@users.noreply.github.com>
2019-02-22 23:49:55 +0100

    now it seems to work

--- smarthome_raspi/app/raspi.py
+++ smarthome_raspi/app/raspi.py
@@ -19,16 +19,14 @@ class RaspiTemp:
         jsons = []
         result = subprocess.run("cat /sys/class/thermal/thermal_zone0/temp", shell=True, capture_output=True)
         temp_cpu_raw = int(result.stdout)
-        #temp_cpu = "{}.{}".format(int(temp_cpu_raw) / 1000, (int(temp_cpu_raw)/1000) % (int(temp_cpu_raw)/100))
+        temp_cpu = "{}.{}".format(temp_cpu_raw / 1000, (temp_cpu_raw/1000) % (temp_cpu_raw/100))
 
         freq0_cpu = subprocess.run("cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq", shell=True, capture_output=True).stdout
         freq1_cpu = subprocess.run("cat /sys/devices/system/cpu/cpu1/cpufreq/scaling_cur_freq", shell=True, capture_output=True).stdout
         freq2_cpu = subprocess.run("cat /sys/devices/system/cpu/cpu2/cpufreq/scaling_cur_freq", shell=True, capture_output=True).stdout
         freq3_cpu = subprocess.run("cat /sys/devices/system/cpu/cpu3/cpufreq/scaling_cur_freq", shell=True, capture_output=True).stdout
 
-        print(result)
-        print(temp_cpu_raw)
-        #print(temp_cpu)
+        print(temp_cpu)
         print(freq0_cpu)
 
         jsons.append({
@@ -37,7 +35,7 @@ class RaspiTemp:
                 "host": config.hostname
             },
             "fields": {
-                "temp": 0
+                "temp": temp_cpu
             },
             "time": datetime.datetime.now()
         })
