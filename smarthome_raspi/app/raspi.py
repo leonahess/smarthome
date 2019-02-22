f6a8fb33e9b4460ef6ee25dfeb709bd655159b34
Leon <32615985+leonhess@users.noreply.github.com>
2019-02-22 23:16:03 +0100

    fixed something

--- smarthome_raspi/app/raspi.py
+++ smarthome_raspi/app/raspi.py
@@ -18,14 +18,14 @@ class RaspiTemp:
 
     def read(self):
         jsons = []
-        result = subprocess.run("cat /sys/class/thermal/thermal_zone0/temp")
+        result = subprocess.run("cat /sys/class/thermal/thermal_zone0/temp", shell=True)
         temp_cpu_raw = result.stdout
         temp_cpu = float("{}.{}".format(temp_cpu_raw / 1000, temp_cpu_raw / 100))
 
-        freq0_cpu = float(subprocess.run("cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq").stdout)
-        freq1_cpu = float(subprocess.run("cat /sys/devices/system/cpu/cpu1/cpufreq/scaling_cur_freq").stdout)
-        freq2_cpu = float(subprocess.run("cat /sys/devices/system/cpu/cpu2/cpufreq/scaling_cur_freq").stdout)
-        freq3_cpu = float(subprocess.run("cat /sys/devices/system/cpu/cpu3/cpufreq/scaling_cur_freq").stdout)
+        freq0_cpu = float(subprocess.run("cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq", shell=True).stdout)
+        freq1_cpu = float(subprocess.run("cat /sys/devices/system/cpu/cpu1/cpufreq/scaling_cur_freq", shell=True).stdout)
+        freq2_cpu = float(subprocess.run("cat /sys/devices/system/cpu/cpu2/cpufreq/scaling_cur_freq", shell=True).stdout)
+        freq3_cpu = float(subprocess.run("cat /sys/devices/system/cpu/cpu3/cpufreq/scaling_cur_freq", shell=True).stdout)
 
         jsons.append({
             "measurement": "cpu",
