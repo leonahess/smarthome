653f98c1b25cc2ed6d71a9a5c3920be9ba34cc8e
Leon <32615985+leonhess@users.noreply.github.com>
2019-03-26 00:36:31 +0100

    more website stuff

--- website/app/routes.py
+++ website/app/routes.py
@@ -50,94 +50,48 @@ def table():
     return render_template('sensors.html', temp_result=temp_result, hum_result=hum_result)
 
 
+def parse(query_result):
+
+    parse_result = []
+
+    for res in query_result.get_points():
+        time = dateutil.parser.parse(res['time'])
+        if res['mean'] is not None:
+            parse_result.append(
+                {
+                    "data": round(res['mean'], 2),
+                    "time": "{}:{}".format(time.hour + 1, time.minute),
+                }
+            )
+
+    return parse_result
+
+
+def query(field, measurement, tag, divisor):
+
+    query_result = influx.query("""SELECT mean("{}") / {} FROM "{}" WHERE ("name" = '{}') AND (time > now() - 1d) 
+        GROUP BY time(10m)fill(0)""".format(field, divisor, measurement, tag))
+
+    return parse(query_result)
+
+
 @app.route('/graphs')
 def graphs():
-    front_outer = []
-    back_outer = []
-    front_board = []
-    back_board = []
-    front_radiator = []
-    back_radiator = []
-
-    front_outer_query = influx.query(
-        """SELECT mean("temperature") FROM "temperature" WHERE ("name" = 'front_window_outside') AND (time > now() - 1d) 
-        GROUP BY time(10m)fill(0)""")
-    back_outer_query = influx.query(
-        """SELECT mean("temperature") FROM "temperature" WHERE ("name" = 'back_window_outside') AND (time > now() - 1d) 
-        GROUP BY time(10m)fill(0)""")
-    front_board_query = influx.query(
-        """SELECT mean("temperature") FROM "temperature" WHERE ("name" = 'front_window_inside') AND (time > now() - 1d) 
-        GROUP BY time(10m)fill(0)""")
-    back_board_query = influx.query(
-        """SELECT mean("temperature") FROM "temperature" WHERE ("name" = 'back_window_inside') AND (time > now() - 1d) 
-        GROUP BY time(10m)fill(0)""")
-    back_radiator_query = influx.query(
-        """SELECT mean("temperature") FROM "temperature" WHERE ("name" = 'back_radiator') AND (time > now() - 1d) 
-        GROUP BY time(10m)fill(0)""")
-    front_radiator_query = influx.query(
-        """SELECT mean("temperature") FROM "temperature" WHERE ("name" = 'front_radiator') AND (time > now() - 1d) 
-        GROUP BY time(10m)fill(0)""")
-
-    for res in front_outer_query.get_points():
-        time = dateutil.parser.parse(res['time'])
-        if res['mean'] is not None:
-            front_outer.append(
-                {
-                    "temperature": round(res['mean'], 2),
-                    "time": "{}:{}".format(time.hour + 1, time.minute),
-                    "name": "front_window_outside"
-                }
-            )
-    for res in back_outer_query.get_points():
-        time = dateutil.parser.parse(res['time'])
-        if res['mean'] is not None:
-            back_outer.append(
-                {
-                    "temperature": round(res['mean'], 2),
-                    "time": "{}:{}".format(time.hour + 1, time.minute),
-                    "name": "front_window_outside"
-                }
-            )
-    for res in front_board_query.get_points():
-        time = dateutil.parser.parse(res['time'])
-        if res['mean'] is not None:
-            front_board.append(
-                {
-                    "temperature": round(res['mean'], 2),
-                    "time": "{}:{}".format(time.hour + 1, time.minute),
-                    "name": "front_window_outside"
-                }
-            )
-    for res in back_board_query.get_points():
-        time = dateutil.parser.parse(res['time'])
-        if res['mean'] is not None:
-            back_board.append(
-                {
-                    "temperature": round(res['mean'], 2),
-                    "time": "{}:{}".format(time.hour + 1, time.minute),
-                    "name": "front_window_outside"
-                }
-            )
-    for res in front_radiator_query.get_points():
-        time = dateutil.parser.parse(res['time'])
-        if res['mean'] is not None:
-            front_radiator.append(
-                {
-                    "temperature": round(res['mean'], 2),
-                    "time": "{}:{}".format(time.hour + 1, time.minute),
-                    "name": "front_window_outside"
-                }
-            )
-    for res in back_radiator_query.get_points():
-        time = dateutil.parser.parse(res['time'])
-        if res['mean'] is not None:
-            back_radiator.append(
-                {
-                    "temperature": round(res['mean'], 2),
-                    "time": "{}:{}".format(time.hour + 1, time.minute),
-                    "name": "front_window_outside"
-                }
-            )
+    front_outer = query("temperature", "temperature", "front_window_outside", 1)
+    back_outer = query("temperature", "temperature", "back_window_outside", 1)
+    front_board = query("temperature", "temperature", "front_window_inside", 1)
+    back_board = query("temperature", "temperature", "back_window_inside", 1)
+    desk = query("temperature", "temperature", "desk", 1)
+    front_radiator = query("temperature", "temperature", "front_radiator", 1)
+    back_radiator = query("temperature", "temperature", "back_radiator", 1)
+    back_board_hum = query("humidity", "humidity", "window_front", 1)
+    front_board_hum = query("humidity", "humidity", "window_back", 1)
+    desk_hum = query("humidity", "humidity", "desk", 1)
+    power_computer = query("milliwatt", "power", "Computer", 1000)
+    power_server = query("milliwatt", "power", "Server", 1000)
+    power_small = query("milliwatt", "power", "Kleinteile", 1000)
 
     return render_template('graphs.html', front_outer=front_outer, back_outer=back_outer, front_board=front_board,
-                           back_board=back_board, front_radiator=front_radiator, back_radiator=back_radiator)
+                           back_board=back_board, front_radiator=front_radiator, back_radiator=back_radiator, desk=desk,
+                           back_board_hum=back_board_hum, front_board_hum=front_board_hum, desk_hum=desk_hum,
+                           power_computer=power_computer, power_server=power_server, power_small=power_small)
